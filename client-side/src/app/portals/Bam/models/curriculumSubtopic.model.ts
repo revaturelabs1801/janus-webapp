f2e5d73d437d93f264ddf985d629144df9f734f1
Daniel <drobin897@gmail.com>
2018-02-03 17:12:53 -0500

    committing to pull

--- client-side/src/app/portals/Bam/models/curriculumSubtopic.model.ts
+++ client-side/src/app/portals/Bam/models/curriculumSubtopic.model.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:client-side/src/app/portals/Bam/models/curriculumSubtopic.model.ts
-import { SubtopicName } from './subtopicname.model';
-import { Curriculum } from './curriculum.model';
-=======
 import { Curriculum } from './curriculum.model';
 import { SubtopicName } from './subtopicname.model';
->>>>>>> 99f32ecd1289adaccc60933ff3bdf0bd76a13b98:client-side/src/app/portals/Bam/models/curriculumSubtopic.model.ts
 
 export class CurriculumSubtopic {
     curriculumSubtopicId: number;
