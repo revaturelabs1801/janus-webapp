d41fd67eabd1c9ae718d928e907d1ebf48565309
Carter Taylor <carterrenwick@outlook.com>
2018-02-03 18:11:14 -0500

    added new course-structure component

--- client-side/src/app/portals/Bam/bam.module.ts
+++ client-side/src/app/portals/Bam/bam.module.ts
@@ -5,12 +5,13 @@ import { BamRoutingModule } from './bam-routing.module';
 import { BamComponent } from './bam.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainCurriculumViewComponent } from './components/curriculum-editor/main-curriculum-view/main-curriculum-view.component';
+import { CourseStructureComponent } from './components/curriculum-editor/course-structure/course-structure.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BamRoutingModule
   ],
-  declarations: [BamComponent, HomeComponent, MainCurriculumViewComponent]
+  declarations: [BamComponent, HomeComponent, MainCurriculumViewComponent, CourseStructureComponent]
 })
 export class BamModule { }
---
+++ client-side/src/app/portals/Bam/components/curriculum-editor/course-structure/course-structure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-structure',
+  templateUrl: './course-structure.component.html',
+  styleUrls: ['./course-structure.component.css']
+})
+export class CourseStructureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
