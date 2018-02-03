cc5c1a427f69a85803915c2bc63a037bb8e797fe
mmalhind <mmalhind@umich.edu>
2018-02-03 17:54:31 -0500

    routing to curriculum editor working

--- client-side/src/app/portals/Bam/bam.module.ts
+++ client-side/src/app/portals/Bam/bam.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { BamRoutingModule } from './bam-routing.module';
 import { BamComponent } from './bam.component';
 import { HomeComponent } from './components/home/home.component';
+import { MainCurriculumViewComponent } from './components/curriculum-editor/main-curriculum-view/main-curriculum-view.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BamRoutingModule
   ],
-  declarations: [BamComponent, HomeComponent]
+  declarations: [BamComponent, HomeComponent, MainCurriculumViewComponent]
 })
 export class BamModule { }
