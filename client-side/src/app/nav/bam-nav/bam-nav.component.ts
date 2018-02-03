87564dc70d61b1561f3b8afd37b1524a6a13f95d
Allan <allanjpoindexter@outlook.com>
2018-02-03 12:54:15 -0500

    Adds a bam-nav component within app/nav. 	* Navigational buttons that routerLinks that follow a 	  '/Bam/location' convention. These may be replaced later. 	* Adds an import for the BamUser model to display the user's 	  and role 	* Adds a collapsed property and collapse EventEmitter for 	  closing the dropdown lists when the user clicks a navigation 	  link.
    Adds an *ngIf Directive within the main NavComponent for displaying
    Bam's navigational bar, then imports the BamNavComponent within the
    module.
    
    Adds an Entry for Bam within the janusGlobal.ts file, so the App
    selector will show 'Janus' instead of 'Bam'.

---
+++ client-side/src/app/nav/bam-nav/bam-nav.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BamUser } from '../../portals/Bam/models/bamuser.modal';
+
+@Component({
+  selector: 'app-bam-nav',
+  templateUrl: './bam-nav.component.html',
+  styleUrls: ['./bam-nav.component.css']
+})
+export class BamNavComponent implements OnInit {
+
+  @Input() collapsed: boolean;
+  @Output() collapse: EventEmitter<any> = new EventEmitter<any>();
+
+  bamUser: BamUser;
+
+  constructor(/* Some User Service*/) {
+    // Set the current User
+  }
+
+  ngOnInit() {
+  }
+
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
+    this.collapse.emit(this.collapsed);
+  }
+}
