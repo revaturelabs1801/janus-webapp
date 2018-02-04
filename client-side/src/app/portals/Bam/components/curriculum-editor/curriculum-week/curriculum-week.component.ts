3b2805b17243e5eaf97051eff854302978386227
Daniel <drobin897@gmail.com>
2018-02-04 18:53:45 -0500

    begun view of weeks

--- client-side/src/app/portals/Bam/components/curriculum-editor/curriculum-week/curriculum-week.component.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/curriculum-week/curriculum-week.component.ts
@@ -9,14 +9,20 @@ import { Component, OnInit } from '@angular/core';
 
 export class CurriculumWeekComponent implements OnInit {
 
-
+  weeks: string[][];
+  weekOne: string[];
+  weekTwo: string[];
+  weekThree: string[];
 
 
   constructor() { }
 
   ngOnInit() {
+    this.weekOne = ['W1', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    this.weekTwo = ['W2', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    this.weekThree = ['W3', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    this.weeks = [this.weekOne, this.weekTwo, this.weekThree];
 
-    let weeks: string[] = ['One', 'Two', 'Three', 'Four'];
   }
 
 }
