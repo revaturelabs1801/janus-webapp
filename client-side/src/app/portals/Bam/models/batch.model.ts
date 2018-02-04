4183bb10d85f1b8b218ac27a4794b48bb56b306f
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-02-04 12:00:25 -0500

    tslint removing to be consistent.

--- client-side/src/app/portals/Bam/models/batch.model.ts
+++ client-side/src/app/portals/Bam/models/batch.model.ts
@@ -1,18 +1,17 @@
-import { BatchType } from "./batch-type.model";
-
+import { BatchType } from './batch-type.model';
 
 export class Batch {
-    id : number; 
-    name: string; 
-    startDate : Date; 
-    endDate : Date;
-    type : BatchType;     
+    id: number;
+    name: string;
+    startDate: Date;
+    endDate: Date;
+    type: BatchType;
 
-    constructor(id : number, name : string, startDate: Date, endDate : Date, type : BatchType ){
-        this.id = id; 
-        this.name = name; 
-        this.startDate = startDate; 
-        this.endDate = endDate; 
+    constructor(id: number, name: string, startDate: Date, endDate: Date, type: BatchType ) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 }
 
@@ -22,24 +21,3 @@ export class Batch {
 //     "name": "1701-java-batch",
 //     "startDate": 1483938000000,
 //     "endDate": 1489723200000,
-//     "trainer": {
-//         "userId": 7,
-//         "fName": "Jonathan",
-//         "mName": "L",
-//         "lName": "Joseph",
-//         "email": "jn@mail.com",
-//         "pwd": "$2a$10$0NfT1vNaTR0gLuHVxoe.U.tjMErqcKJrpwfWd8FSoXrZ/8BLKmocq",
-//         "role": 2,
-//         "batch": null,
-//         "phone": "407",
-//         "phone2": null,
-//         "skype": "lol@skype",
-//         "pwd2": null,
-//         "assignForceID": null
-//     },
-//     "type": {
-//         "id": 1,
-//         "name": "Java",
-//         "length": 10
-//     }
-// }
---
+++ client-side/src/app/portals/Bam/models/trainer.model.ts
@@ -0,0 +1,31 @@
+export class Trainer {
+    userId: number;
+    fName: string;
+    mName: string;
+    lName: string;
+    email: string;
+    pwd: string;
+    role: number;
+    batch: number;
+    phone: string;
+    phone2: string;
+    skype: string;
+    pwd2: string;
+    assignForceID: number;
+}
+
+//     "trainer": {
+//         "userId": 7,
+//         "fName": "Jonathan",
+//         "mName": "L",
+//         "lName": "Joseph",
+//         "email": "jn@mail.com",
+//         "pwd": "$2a$10$0NfT1vNaTR0gLuHVxoe.U.tjMErqcKJrpwfWd8FSoXrZ/8BLKmocq",
+//         "role": 2,
+//         "batch": null,
+//         "phone": "407",
+//         "phone2": null,
+//         "skype": "lol@skype",
+//         "pwd2": null,
+//         "assignForceID": null
+//     },
