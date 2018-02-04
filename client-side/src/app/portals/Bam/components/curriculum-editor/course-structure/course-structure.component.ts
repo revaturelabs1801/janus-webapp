ad0c111f553319e8e44a704d5da84bd8b0c0fb8b
Carter Taylor <carterrenwick@outlook.com>
2018-02-03 20:14:17 -0500

    working on displaying accordion for curriculums in course-structure-view

--- client-side/src/app/portals/Bam/components/curriculum-editor/course-structure/course-structure.component.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/course-structure/course-structure.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Curriculum } from '../../../models/curriculum.model';
 
 @Component({
   selector: 'app-course-structure',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseStructureComponent implements OnInit {
 
+  dummyData = [new Curriculum(1, 'Java', 1, null, null, '02/03/2018', 7, 1),
+    new Curriculum(2, 'Spring', 1, null, null, '02/03/2018', 7, 1),
+    new Curriculum(3, '.net', 1, null, null, '02/03/2018', 7, 1)];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(event) {
+    //logic to show/hide accordian
   }
 
 }
