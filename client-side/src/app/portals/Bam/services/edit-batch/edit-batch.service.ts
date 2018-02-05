e0616ce6c533b134e49ddcf434a0bc937486c559
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-02-04 20:45:07 -0500

    fixing and removing comments in the services. Will be adding more to this.

--- client-side/src/app/portals/Bam/services/edit-batch/edit-batch.service.ts
+++ client-side/src/app/portals/Bam/services/edit-batch/edit-batch.service.ts
@@ -1,94 +1,57 @@
 import { Injectable } from '@angular/core';
-import { Batch } from '../../models/batch.model';
 import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+
+import { Batch } from '../../models/batch.model';
+import { BatchType } from '../../models/batch-type.model';
+
+const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable()
 export class EditBatchService {
 
-  //batch : Batch; 
 
   constructor(private http: HttpClient) {
-    //TODO: get current_batch_id from 
-    console.log("Service is constructed"); 
+    // TODO: get current_batch_id from
   }
 
-  getBatchById(batchId : number) :Observable<Batch> {
-    console.log("getBatchById is called"); 
-    let httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-      params:  new HttpParams().set('batchId', batchId.toString())
-    }
-
-    return this.http.get<Batch>("http://localhost:9001/api/v1/batches/byid?batchId=4"); 
-    // get the batch from the server by the id.
-    // url: "rest/api/v1/Batches/ById",
-    // params:{
-    //   batchId: batchId
-    // }
+  getBatchById(batchId: number): Observable<Batch> {
+    return this.http.get<Batch>('http://localhost:9001/api/v1/batches/byid?batchId=' + batchId, httpOptions);
   }
 
-  getUsersInBatch(batchId: number) {
-    //TODO return user
-    console.log("getBatchById is called"); 
-    let httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-      params:  new HttpParams().set('batchId', batchId.toString())
-    }
-
-    return this.http.get<any>("http://localhost:9001/api/v1/users/inbatch?batchId=4"); 
-
-    // get users in the batch by the batchId
-    // url: "rest/api/v1/Users/InBatch"
-    // params: {
-    //   batchId: batchId
-    // }
+  getUsersInBatch(batchId: number): Observable<Batch[]> {
+    return this.http.get<Batch[]>('http://localhost:9001/api/v1/users/inbatch?batchId=' + batchId, httpOptions);
   }
 
-  getUsersNotInBatch(batchId: number) {
-    // TODO: return users
-    // get users who are not in a batch
-    // url: "rest/api/v1/Users/NotInABatch",
-    console.log("getBatchById is called"); 
-    let httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-      params:  new HttpParams().set('batchId', batchId.toString())
-    }
-
-    return this.http.get<any>("http://localhost:9001/api/v1/users/notinabatch?batchId=4"); 
-    // 
+  getUsersNotInBatch(batchId: number): Observable<Batch[]> {
+    return this.http.get<Batch[]>('http://localhost:9001/api/v1/users/notinabatch?batchId=' + batchId, httpOptions);
   }
 
   updateBatch(batch: Batch) {
-    // url: "rest/api/v1/Batches/UpdateBatch",
+    // TODO url: "rest/api/v1/Batches/UpdateBatch",
   }
 
-  getAllBatchTypes() : Observable<any> {
-    console.log("getBatchById is called"); 
-    let httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    }
-
-    return this.http.get<Batch>("http://localhost:9001/api/v1/batches/batchtypes", httpOptions); 
-    //Get all batchtypes 
-    // url: "rest/api/v1/Batches/BatchTypes",
+  getAllBatchTypes(): Observable<BatchType[]> {
+    return this.http.get<BatchType[]>('http://localhost:9001/api/v1/batches/batchtypes', httpOptions);
   }
 
   addUserToBatch() {
-    // This function adds a user to the batch
+    // TODO This function adds a user to the batch
     // url: "rest/api/v1/Users/Add",
   }
 
   removeUserFromBatch() {
-    // This function removes a user from a batch
-    // url: "rest/api/v1/Users/Remove", 
+    // TODO This function removes a user from a batch
+    // url: "rest/api/v1/Users/Remove",
     // params: {
     //   userId: id
     // }
   }
 
   dropAssociateFromBatch() {
-    // This function is used to drop an associate
+    // TODO This function is used to drop an associate
     // url: "rest/api/v1/Users/Drop",
   }
 
