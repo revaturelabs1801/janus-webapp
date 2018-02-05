0ca90d7e5b9e830ca39af5b8efd63ef8d4001076
Daniel <drobin897@gmail.com>
2018-02-04 21:29:53 -0500

    weeks are displayed

--- client-side/src/app/portals/Bam/components/curriculum-editor/curriculum-week/curriculum-week.component.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/curriculum-week/curriculum-week.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeeksDTO } from '../../../models/weeksDTO.model';
+import { CurriculumSubtopic } from '../../../models/curriculumSubtopic.model';
 
 @Component({
   selector: 'app-curriculum-week',
@@ -9,20 +11,40 @@ import { Component, OnInit } from '@angular/core';
 
 export class CurriculumWeekComponent implements OnInit {
 
-  weeks: string[][];
-  weekOne: string[];
-  weekTwo: string[];
-  weekThree: string[];
-
+weekDTO: CurriculumSubtopic[];
+monday: CurriculumSubtopic[] = [];
+tuesday: CurriculumSubtopic[] = [];
+wednesday: CurriculumSubtopic[] = [];
+thursday: CurriculumSubtopic[] = [];
+friday: CurriculumSubtopic[] = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.weekOne = ['W1', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    this.weekTwo = ['W2', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    this.weekThree = ['W3', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    this.weeks = [this.weekOne, this.weekTwo, this.weekThree];
-
+ this.sortSubtopics();
   }
+
+sortSubtopics() {
+this.weekDTO.forEach(elem => {
+switch (elem.curriculumSubtopicDay) {
+  case 1:
+    this.monday.push(elem);
+    break;
+  case 2:
+    this.tuesday.push(elem);
+    break;
+  case 3:
+    this.wednesday.push(elem);
+    break;
+  case 4:
+    this.thursday.push(elem);
+    break;
+  case 5:
+    this.friday.push(elem);
+    break;
+}
+});
+
+}
 
 }
