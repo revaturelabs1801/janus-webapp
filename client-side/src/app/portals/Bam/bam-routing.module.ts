1f3d39036eb28409d376a96e0a4ddd49a26633be
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-02-04 20:47:42 -0500

    moving components to a different branch.

--- client-side/src/app/portals/Bam/bam-routing.module.ts
+++ client-side/src/app/portals/Bam/bam-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BamComponent } from './bam.component';
 import { HomeComponent } from './components/home/home.component';
-import { EditBatchComponent } from './components/edit-batch/edit-batch.component';
 
 const routes: Routes = [
   {
@@ -12,10 +11,6 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent
-      },
-      {
-        path: 'editBatch',
-        component: EditBatchComponent
       },
       {
         path: '**',
