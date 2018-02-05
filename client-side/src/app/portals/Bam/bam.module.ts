f8d465544038d1587fcaf852cf534a7268fb756f
Mohamed <mhswelam@outlook.com>
2018-02-05 14:34:36 -0500

    Welcome view files

--- client-side/src/app/portals/Bam/bam.module.ts
+++ client-side/src/app/portals/Bam/bam.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { BamRoutingModule } from './bam-routing.module';
 import { BamComponent } from './bam.component';
 import { HomeComponent } from './components/home/home.component';
+import { WelcomeComponent } from './components/dashboard/welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
+import { BatchService } from './services/batch.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    BamRoutingModule
+    BamRoutingModule,
+    FormsModule
   ],
-  declarations: [BamComponent, HomeComponent]
+  declarations: [BamComponent, HomeComponent, WelcomeComponent],
+  providers: [BatchService]
 })
 export class BamModule { }
