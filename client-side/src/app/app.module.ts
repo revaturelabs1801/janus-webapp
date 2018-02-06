3d51113be779637d251c325ff5296a9801137462
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-02-06 11:50:18 -0500

    Update app.module.ts
    added a space

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
