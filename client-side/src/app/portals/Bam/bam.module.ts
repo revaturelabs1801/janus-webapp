eb585a5cd5a1143969f9b6f00a3090f6ff4f255b
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-02-06 16:08:50 -0500

    added dragndrop

--- client-side/src/app/portals/Bam/bam.module.ts
+++ client-side/src/app/portals/Bam/bam.module.ts
@@ -15,9 +15,11 @@ import { UsersService } from './services/users.service';
 import { MainCurriculumViewComponent } from './components/curriculum-editor/main-curriculum-view/main-curriculum-view.component';
 import { TopicPoolComponent } from './components/curriculum-editor/topic-pool/topic-pool.component';
 import { CurriculumService } from './services/curriculum.service';
+import { DragDropModule } from 'primeng/dragdrop';
 
 @NgModule({
   imports: [
+    DragDropModule,
     CommonModule,
     BamRoutingModule,
     FormsModule
@@ -28,7 +30,7 @@ import { CurriculumService } from './services/curriculum.service';
     EditBatchComponent,
     AddAssociateToBatchComponent,
     RemoveAssociateFromBatchComponent,
-    MainCurriculumViewComponent, 
+    MainCurriculumViewComponent,
     TopicPoolComponent,
     SearchPipe
   ],
