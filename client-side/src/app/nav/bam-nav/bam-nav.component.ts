958e4b359badfe5600f3b36718077f02b73c7be5
Allan <allanjpoindexter@outlook.com>
2018-02-06 21:04:31 -0500

    Adds the trainer's name and role

--- client-side/src/app/nav/bam-nav/bam-nav.component.ts
+++ client-side/src/app/nav/bam-nav/bam-nav.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BamUser } from '../../portals/Bam/models/bamuser.model';
+import {SessionService} from '../../portals/Bam/services/session.service';
+import { UsersService } from '../../portals/Bam/services/users.service';
+
 
 @Component({
   selector: 'app-bam-nav',
   templateUrl: './bam-nav.component.html',
-  styleUrls: ['./bam-nav.component.css']
+  styleUrls: ['./bam-nav.component.css'],
+  providers: [SessionService, UsersService]
 })
 export class BamNavComponent implements OnInit {
 
@@ -12,12 +16,24 @@ export class BamNavComponent implements OnInit {
   @Output() collapse: EventEmitter<any> = new EventEmitter<any>();
 
   bamUser: BamUser;
+  roleTitle: string;
 
-  constructor(/* Some User Service*/) {
-    // Set the current User
+  constructor(private sessionService: SessionService) {
+    this.bamUser = sessionService.bamUser;
   }
 
   ngOnInit() {
+    switch (this.bamUser.role) {
+        case 1:
+          this.roleTitle = 'Associate';
+          break;
+        case 2:
+          this.roleTitle = 'Trainer';
+          break;
+        case 3:
+          this.roleTitle = 'Quality Control';
+          break;
+    }
   }
 
   toggleCollapse() {
