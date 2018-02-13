b8f2c74512395d38207b16f1e61edae8d7eae684
Destinatus <destinatus@live.com>
2018-02-13 11:43:37 -0500

    bug fixed subtopic progress bar

--- client-side/src/app/portals/Bam/components/dashboard/batch-progress-bar/batch-progress-bar.component.ts
+++ client-side/src/app/portals/Bam/components/dashboard/batch-progress-bar/batch-progress-bar.component.ts
@@ -64,7 +64,7 @@ export class BatchProgressBarComponent implements OnInit, OnChanges {
           if ((this.currentDate.valueOf() - this.batch.startDate.valueOf() >
               this.batch.endDate.valueOf() - this.batch.startDate.valueOf())) {
               this.completedDate = this.batch.endDate.valueOf() - this.batch.startDate.valueOf();
-          }else {
+          }else { 
             this.completedDate = (this.currentDate.valueOf() - this.batch.startDate.valueOf());
           }
           this.percentCompleted = Math.floor(this.completedDate.valueOf() /
@@ -72,10 +72,12 @@ export class BatchProgressBarComponent implements OnInit, OnChanges {
         });
       this.subTopicObs.subscribe(
         data2 => {
+          this.subTopics=null;
           this.subTopics = data2;
+          
           this.topicArray = [];
           if (this.subTopics == null) {
-            this.subTopicCompleted = 0;
+            this.subTopicCompleted = null;
             this.subTopicMissed = 0;
           }else {
             this.subTopicMissed = 0;
